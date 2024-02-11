1c6c60ec64215e879a22e27814115e7353d4b5e2
Ilyin <ilyin.artem2005@mail.ru>
2024-02-11 19:59:11 +0300

    index on main: d609956 first commit

---
+++ BigInt/BIgInt.cpp
@@ -0,0 +1,5 @@
+//
+// Created by artem on 09.02.2024.
+//
+
+#include "BIgInt.h"
---
+++ BigInt/BIgInt.h
@@ -0,0 +1,16 @@
+//
+// Created by artem on 09.02.2024.
+//
+
+#ifndef BIGFLOAT_BIGINT_H
+#define BIGFLOAT_BIGINT_H
+
+
+
+class BIgInt {
+
+};
+
+
+
+#endif //BIGFLOAT_BIGINT_H
